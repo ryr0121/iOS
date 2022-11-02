9e07cbd693c98907426f6bf68fec15cb7d5fadb4
ryr0121 <ryr0121@gmail.com>
2022-11-02 20:46:29 +0900

    Week1 Practice

---
+++ Week1/Week1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by 김초원 on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        
+        
+    }
+
+}
+
