d9fb57d77fb070e9f809728c40f2f5546cdc7c7b
ryr0121 <ryr0121@gmail.com>
2022-11-02 21:20:31 +0900

    Week2 Mission

---
+++ TvingClone/TvingClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TvingClone
+//
+//  Created by 김초원 on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        self.view.backgroundColor = UIColor.black
+
+    }
+
+
+}
+
